ee2a32354409353da5f005e674d913086c7d02db
Necdet Can Ateşman <can.atesman@strg.at>
2018-01-10 17:28:41 +0100

    New: Add two more unit tests for await()

---
+++ test/await.py
@@ -0,0 +1,29 @@
+from score.asyncio import init
+import asyncio
+
+
+def test_stopped_loop():
+    task_done = False
+
+    @asyncio.coroutine
+    def task():
+        nonlocal task_done
+        task_done = True
+
+    mod = init({})
+    mod.await(task())
+    assert task_done
+
+
+def test_running_loop():
+    task_done = False
+
+    @asyncio.coroutine
+    def task():
+        nonlocal task_done
+        task_done = True
+
+    mod = init({})
+    with mod.start_loop():
+        mod.await(task())
+        assert task_done
